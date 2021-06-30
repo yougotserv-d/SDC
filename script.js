3e75639cf0c3b5e2b39dc0fe36518c4cf05a7034
Spencer Asavadejkajorn <spencerasava@gmail.com>
2021-06-30 14:10:00 -0700

    FINAL FOR DOCKER CONTAINER DEPLOYMENT

--- script.js
+++ script.js
@@ -1,19 +1,19 @@
 import http from 'k6/http';
 import { sleep, check } from 'k6';
 export let options = {
-  vus: 1000,
+  vus: 100,
   duration: '10s'
 }
 
 export default function () {
   // console.time('this')
-  // for (var id = 1; id <= 10; id++) {
-  //   http.get(`http://localhost:8080/reviews/meta/?product_id=${id}`);
-  // }
-  let res = http.get('http://localhost:8080/reviews/?product_id=5614');
-  check(res, {
-    'is status 200': (r) => r.status === 200,
-  })
+  for (var id = 1; id <= 10; id++) {
+    http.get(`http://localhost:8080/reviews/meta/?product_id=${id}`);
+  }
+  // let res = http.get('http://localhost:8080/reviews/?product_id=5614');
+  // check(res, {
+  //   'is status 200': (r) => r.status === 200,
+  // })
   // console.timeEnd('this')
   sleep(1);
 }
